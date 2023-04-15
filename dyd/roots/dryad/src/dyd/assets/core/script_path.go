9f71b5626d08f1d998ac1f3014d3cd59930bce3c
socks <somesocks@gmail.com>
2023-04-16 00:07:23 +0200

    - adding core ScriptPath function - adding `dryad script path` command

---
+++ dyd/roots/dryad/src/dyd/assets/core/script_path.go
@@ -0,0 +1,22 @@
+package core
+
+import (
+	"fmt"
+)
+
+type ScriptPathRequest struct {
+	BasePath string
+	Scope    string
+	Setting  string
+}
+
+func ScriptPath(request ScriptPathRequest) (string, error) {
+	runPath, err := ScopeSettingPath(request.BasePath, request.Scope, request.Setting)
+	if err != nil {
+		return "", err
+	} else if runPath == "" {
+		return "", fmt.Errorf("[error] %s not found in scope %s", request.Setting, request.Scope)
+	} else {
+		return runPath, err
+	}
+}
--- dyd/roots/dryad/src/dyd/assets/main.go
+++ dyd/roots/dryad/src/dyd/assets/main.go
@@ -748,7 +748,57 @@ func _buildCLI() cli.App {
 		WithArg(cli.NewArg("-- args", "args to pass to the script").AsOptional()).
 		WithAction(scriptRunAction)
 
+	var scriptPathAction = func(req cli.ActionRequest) int {
+		var command = req.Args[0]
+		var options = req.Opts
+
+		basePath, err := os.Getwd()
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		var scope string
+		if options["scope"] != nil {
+			scope = options["scope"].(string)
+		} else {
+			var err error
+			scope, err = dryad.ScopeGetDefault(scope)
+			fmt.Println("[info] loading default scope:", scope)
+			if err != nil {
+				log.Fatal(err)
+			}
+		}
+
+		// if the scope is unset, bypass expansion and run the action directly
+		if scope == "" || scope == "none" {
+			log.Fatal("no scope set, can't find command")
+		} else {
+			fmt.Println("[info] using scope:", scope)
+		}
+
+		scriptPath, err := dryad.ScriptPath(dryad.ScriptPathRequest{
+			BasePath: basePath,
+			Scope:    scope,
+			Setting:  "script-run-" + command,
+		})
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		fmt.Println(scriptPath)
+
+		return 0
+	}
+
+	scriptPathAction = _scopeHandler(scriptPathAction)
+
+	var scriptPath = cli.NewCommand("path", "print the path to a script").
+		WithArg(cli.NewArg("command", "the script name").WithType(cli.TypeString)).
+		WithOption(cli.NewOption("scope", "set the scope for the command")).
+		WithAction(scriptPathAction)
+
 	var script = cli.NewCommand("script", "commands to work with a scoped script").
+		WithCommand(scriptPath).
 		WithCommand(scriptRun)
 
 	var run = cli.NewCommand("run", "alias for `dryad script run`").
