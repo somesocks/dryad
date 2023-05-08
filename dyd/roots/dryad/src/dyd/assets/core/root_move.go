44f31f33f2293c1bcb858081750fd7aa9db0b1e1
socks <somesocks@gmail.com>
2023-05-08 22:27:48 +0200

    - adding `RootMove` to core - adding `dryad root move` command

---
+++ dyd/roots/dryad/src/dyd/assets/core/root_move.go
@@ -0,0 +1,36 @@
+package core
+
+import (
+	"os"
+)
+
+func RootMove(sourcePath string, destPath string) error {
+
+	// normalize the source path
+	sourcePath, err := RootPath(sourcePath)
+	if err != nil {
+		return err
+	}
+
+	// normalize the replacement path
+	destPath, err = RootPath(destPath)
+	if err != nil {
+		return err
+	}
+
+	// copy the root to the new path
+	err = RootCopy(sourcePath, destPath)
+	if err != nil {
+		return err
+	}
+
+	// replace references to the root
+	err = RootReplace(sourcePath, destPath)
+	if err != nil {
+		return err
+	}
+
+	// delete the old root
+	err = os.RemoveAll(sourcePath)
+	return err
+}
