a837511b2630d77ed06c71cf0877a68750495017
socks <somesocks@gmail.com>
2023-05-09 20:29:05 +0200

    - adding `RootUnlink` to core - adding `dryad root unlink` command - adding docs page for `dryad root unlink`

---
+++ docs/usage/cli/dryad-root-unlink.md
@@ -0,0 +1,19 @@
+---
+title: dryad root unlink
+layout: default
+parent: CLI
+grand_parent: Usage
+---
+
+# dryad root unlink
+
+```
+$ dryad root unlink --help
+dryad root unlink <path>
+
+Description:
+    remove a dependency linked to the current root
+
+Arguments:
+    path   path to the dependency to unlink
+```
--- dyd/roots/dryad/src/dyd/assets/main.go
+++ dyd/roots/dryad/src/dyd/assets/main.go
@@ -292,6 +292,26 @@ func _buildCLI() cli.App {
 			return 0
 		})
 
+	var rootUnlink = cli.NewCommand("unlink", "remove a dependency linked to the current root").
+		WithArg(cli.NewArg("path", "path to the dependency to unlink")).
+		WithAction(func(req cli.ActionRequest) int {
+			var args = req.Args
+
+			var rootPath, err = os.Getwd()
+			if err != nil {
+				log.Fatal(err)
+			}
+
+			var path = args[0]
+
+			err = dryad.RootUnlink(rootPath, path)
+			if err != nil {
+				log.Fatal(err)
+			}
+
+			return 0
+		})
+
 	var rootInit = cli.NewCommand("init", "create a new root directory structure in the current dir").
 		WithArg(cli.NewArg("path", "the path to init the root at. defaults to current directory").AsOptional()).
 		WithAction(func(req cli.ActionRequest) int {
@@ -427,7 +447,8 @@ func _buildCLI() cli.App {
 		WithCommand(rootLink).
 		WithCommand(rootMove).
 		WithCommand(rootPath).
-		WithCommand(rootReplace)
+		WithCommand(rootReplace).
+		WithCommand(rootUnlink)
 
 	var rootsList = cli.NewCommand("list", "list all roots that are dependencies for the current root (or roots of the current garden, if the path is not a root)").
 		WithArg(cli.NewArg("path", "path to the base root (or garden) to list roots in").AsOptional()).
