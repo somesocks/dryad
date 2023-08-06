1794e983a37b61ec77b0526acfcdd669e3a7a168
socks <somesocks@gmail.com>
2023-08-06 10:43:13 +0200

    - removing -d option on xargs for compatibility

--- dyd/roots/publish/cli_docs/dyd/assets/generate_cli_pages.sh
+++ dyd/roots/publish/cli_docs/dyd/assets/generate_cli_pages.sh
@@ -7,4 +7,4 @@ SRC_DIR=$DYD_STEM
 DEST_DIR=$DYD_BUILD
 
 
-dryad system commands | xargs -d '\n' -I {} sh $SRC_DIR/dyd/assets/generate_cli_page.sh "{}"
+dryad system commands | xargs -I {} sh $SRC_DIR/dyd/assets/generate_cli_page.sh "{}"
