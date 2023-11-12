8e100fbf9c3784f0d9aa8e764413b56630187ab0
socks <somesocks@gmail.com>
2023-11-12 22:27:55 +0100

    - adding basic implementation of `dryad roots owning` command

--- dyd/roots/source/dryad/dyd/assets/cli/roots.go
+++ dyd/roots/source/dryad/dyd/assets/cli/roots.go
@@ -6,4 +6,5 @@ import (
 
 var rootsCommand = clib.NewCommand("roots", "commands to work with dryad roots").
 	WithCommand(rootsListCommand).
+	WithCommand(rootsOwningCommand).
 	WithCommand(rootsPathCommand)
