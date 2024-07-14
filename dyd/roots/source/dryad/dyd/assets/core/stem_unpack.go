64a63c6f4378470fd6d851097c61e7f28a4a5463
James Larsen <james.eric.larsen@gmail.com>
2024-07-14 19:15:27 +0200

    - disabling `dryad stem unpack` for now

--- dyd/roots/source/dryad/dyd/assets/core/stem_unpack.go
+++ dyd/roots/source/dryad/dyd/assets/core/stem_unpack.go
@@ -1,93 +1,96 @@
 package core
 
 import (
-	"archive/tar"
-	"compress/gzip"
-	"fmt"
-	"io"
-	"os"
-	"path/filepath"
+	// "archive/tar"
+	// "compress/gzip"
+	// "fmt"
+	// "io"
+	// "os"
+	// "path/filepath"
+	"errors"
 )
 
 func StemUnpack(gardenPath string, packPath string) (string, error) {
-	var err error
+	return "", errors.New("StemUnpack not implemented")
 
-	// clean garden path
-	gardenPath, err = GardenPath(gardenPath)
-	if err != nil {
-		return "", err
-	}
+	// var err error
 
-	// convert relative path to absolute
-	if !filepath.IsAbs(packPath) {
-		wd, err := os.Getwd()
-		if err != nil {
-			return "", err
-		}
-		packPath = filepath.Join(wd, packPath)
-	}
+	// // clean garden path
+	// gardenPath, err = GardenPath(gardenPath)
+	// if err != nil {
+	// 	return "", err
+	// }
 
-	workspacePath, err := os.MkdirTemp("", "dryad-*")
-	if err != nil {
-		return "", err
-	}
-	defer os.RemoveAll(workspacePath)
-	fmt.Println("workspacePath", workspacePath)
+	// // convert relative path to absolute
+	// if !filepath.IsAbs(packPath) {
+	// 	wd, err := os.Getwd()
+	// 	if err != nil {
+	// 		return "", err
+	// 	}
+	// 	packPath = filepath.Join(wd, packPath)
+	// }
 
-	fr, err := os.Open(packPath)
-	if err != nil {
-		return "", err
-	}
+	// workspacePath, err := os.MkdirTemp("", "dryad-*")
+	// if err != nil {
+	// 	return "", err
+	// }
+	// defer os.RemoveAll(workspacePath)
+	// fmt.Println("workspacePath", workspacePath)
 
-	gzr, err := gzip.NewReader(fr)
-	if err != nil {
-		return "", err
-	}
-	defer gzr.Close()
+	// fr, err := os.Open(packPath)
+	// if err != nil {
+	// 	return "", err
+	// }
 
-	tr := tar.NewReader(gzr)
+	// gzr, err := gzip.NewReader(fr)
+	// if err != nil {
+	// 	return "", err
+	// }
+	// defer gzr.Close()
 
-	for {
+	// tr := tar.NewReader(gzr)
 
-		header, err := tr.Next()
-		if err == io.EOF {
-			break
-		} else if err != nil {
-			return "", err
-		}
+	// for {
 
-		path := filepath.Join(workspacePath, header.Name)
-		info := header.FileInfo()
-		if info.IsDir() {
-			if err = os.MkdirAll(path, info.Mode()); err != nil {
-				return "", err
-			}
-		} else {
-			if err = os.MkdirAll(filepath.Dir(path), os.ModePerm); err != nil {
-				return "", err
-			}
+	// 	header, err := tr.Next()
+	// 	if err == io.EOF {
+	// 		break
+	// 	} else if err != nil {
+	// 		return "", err
+	// 	}
 
-			file, err := os.OpenFile(path, os.O_CREATE|os.O_TRUNC|os.O_WRONLY, info.Mode())
-			if err != nil {
-				return "", err
-			}
-			defer file.Close()
-			_, err = io.Copy(file, tr)
-			if err != nil {
-				return "", err
-			}
-		}
-	}
+	// 	path := filepath.Join(workspacePath, header.Name)
+	// 	info := header.FileInfo()
+	// 	if info.IsDir() {
+	// 		if err = os.MkdirAll(path, info.Mode()); err != nil {
+	// 			return "", err
+	// 		}
+	// 	} else {
+	// 		if err = os.MkdirAll(filepath.Dir(path), os.ModePerm); err != nil {
+	// 			return "", err
+	// 		}
 
-	stemFingerprint, err := StemValidate(workspacePath)
-	if err != nil {
-		return "", err
-	}
+	// 		file, err := os.OpenFile(path, os.O_CREATE|os.O_TRUNC|os.O_WRONLY, info.Mode())
+	// 		if err != nil {
+	// 			return "", err
+	// 		}
+	// 		defer file.Close()
+	// 		_, err = io.Copy(file, tr)
+	// 		if err != nil {
+	// 			return "", err
+	// 		}
+	// 	}
+	// }
 
-	_, err = HeapAddStem(gardenPath, workspacePath)
-	if err != nil {
-		return stemFingerprint, err
-	}
+	// stemFingerprint, err := StemValidate(workspacePath)
+	// if err != nil {
+	// 	return "", err
+	// }
 
-	return stemFingerprint, err
+	// _, err = HeapAddStem(gardenPath, workspacePath)
+	// if err != nil {
+	// 	return stemFingerprint, err
+	// }
+
+	// return stemFingerprint, err
 }
