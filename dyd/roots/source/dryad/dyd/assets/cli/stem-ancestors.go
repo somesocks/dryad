f332623194ea123cebccbb848307b28f1df6b6b5
James Larsen <somesocks@gmail.com>
2024-07-23 21:45:48 +0200

    - adding `dryad stem ancestors` command - adding dryad.StemAncestorsWalk core function

---
+++ dyd/roots/source/dryad/dyd/assets/cli/stem-ancestors.go
@@ -0,0 +1,103 @@
+package cli
+
+import (
+	clib "dryad/cli-builder"
+	dryad "dryad/core"
+	fs2 "dryad/filesystem"
+	"os"
+	"path/filepath"
+	// "regexp"
+
+	"fmt"
+
+	zlog "github.com/rs/zerolog/log"
+)
+
+var stemAncestorsCommand = func() clib.Command {
+	command := clib.NewCommand("ancestors", "list all direct and indirect dependencies of a stem").
+		WithArg(
+			clib.
+				NewArg("path", "path to the stem base dir").
+				AsOptional().
+				WithAutoComplete(ArgAutoCompletePath),
+		).
+		WithOption(clib.NewOption("relative", "print stems relative to the base garden path. default true").WithType(clib.OptionTypeBool)).
+		WithOption(clib.NewOption("self", "include the base stem itself. default false").WithType(clib.OptionTypeBool)).
+		WithAction(func(req clib.ActionRequest) int {
+			var args = req.Args
+			var options = req.Opts
+
+			var err error
+			var path string
+			var relative bool = true
+			var self bool = false
+
+			if len(args) > 0 {
+				path = args[0]
+				path, err = filepath.Abs(path)
+				if err != nil {
+					zlog.Fatal().Err(err).Msg("error while cleaning path")
+					return 1
+				}
+			}
+			if path == "" {
+				path, err = os.Getwd()
+				if err != nil {
+					zlog.Fatal().Err(err).Msg("error while finding working directory")
+					return 1
+				}
+			}
+
+			if options["relative"] != nil {
+				relative = options["relative"].(bool)
+			} else {
+				relative = true
+			}
+
+			if options["self"] != nil {
+				self = options["self"].(bool)
+			} else {
+				self = false
+			}
+
+			var gardenPath string
+			gardenPath, err = dryad.GardenPath(path)
+			if err != nil {
+				zlog.Fatal().Err(err).Msg("error while finding garden path")
+				return 1
+			}
+
+			err = dryad.StemAncestorsWalk(
+				dryad.StemAncestorsWalkRequest{
+					BasePath: path,
+					OnMatch: func(context fs2.Walk4Context) error {
+						// calculate the relative path to the root from the base of the garden
+						relPath, err := filepath.Rel(gardenPath, context.Path)
+						if err != nil {
+							return err
+						}
+
+						if relative {
+							fmt.Println(relPath)
+						} else {
+							fmt.Println(context.Path)
+						}
+
+						return nil
+					},
+					Self: self,
+				},
+			)
+			if err != nil {
+				zlog.Fatal().Err(err).Msg("error while listing files")
+				return 1
+			}
+
+			return 0
+		})
+
+	command = LoggingCommand(command)
+	command = HelpCommand(command)
+
+	return command
+}()
---
+++ dyd/roots/source/dryad/dyd/assets/core/stem_ancestors_walk.go
@@ -0,0 +1,117 @@
+package core
+
+import (
+	fs2 "dryad/filesystem"
+	// "os"
+	"path/filepath"
+	"regexp"
+	zlog "github.com/rs/zerolog/log"
+)
+
+var RE_STEM_ANCESTORS_SHOULD_CRAWL = regexp.MustCompile(
+	"^(" +
+		"(\\.)" +
+		"|(dyd)" +
+		"|(dyd/dependencies)" +
+		"|((dyd/dependencies/[^/]*))" +
+		"|((dyd/dependencies/[^/]*/)+dyd)" +
+		"|((dyd/dependencies/[^/]*/)+dyd/dependencies)" +
+		"|((dyd/dependencies/[^/]*/)+dyd/dependencies/[^/]*)" +
+		")$",
+)
+
+// should walk
+
+func StemAncestorsWalkCrawler() func(fs2.Walk4Context) (bool, error) {
+
+	var crawlMap = make(map[string]bool)
+
+	// - if the vpath matches the pattern then yes
+	return func (context fs2.Walk4Context) (bool, error) {
+		// don't crawl a path that's already been crawled
+		if _, seen := crawlMap[context.Path]; seen {
+			return false, nil
+		}
+
+		var relPath, relErr = filepath.Rel(context.BasePath, context.VPath)
+		if relErr != nil {
+			return false, relErr
+		}
+		matchesPath := RE_STEM_ANCESTORS_SHOULD_CRAWL.Match([]byte(relPath))
+
+		isDir := context.Info.IsDir()
+		if isDir {
+			crawlMap[context.Path] = true
+		}
+
+		zlog.Trace().
+			Str("relPath", relPath).
+			Bool("crawl", matchesPath).
+			Msg("stem ancestors walk / should crawl")
+
+		return matchesPath, nil
+	}
+}
+
+
+
+var RE_STEM_ANCESTORS_WALK_SHOULD_MATCH = regexp.MustCompile(
+	"^(" +
+		"(\\.)" +
+		"|((dyd/dependencies/[^/]*))" +
+		"|((dyd/dependencies/[^/]*/)+dyd/dependencies/[^/]*)" +
+	")$",
+)
+
+// should match
+func StemAncestorsWalkShouldMatch(context fs2.Walk4Context) (bool, error) {
+	// fmt.Println("[debug] StemWalkShouldMatch", context.VPath)
+
+	var relPath, relErr = filepath.Rel(context.BasePath, context.VPath)
+	if relErr != nil {
+		return false, relErr
+	}
+	matchesPath := RE_STEM_ANCESTORS_WALK_SHOULD_MATCH.Match([]byte(relPath))
+
+	isDir := context.Info.IsDir()
+
+
+	zlog.Trace().
+		Str("relPath", relPath).
+		Bool("match", matchesPath).
+		Bool("isDir", isDir).
+		Msg("stem ancestors walk / should match")
+
+	return matchesPath && isDir, nil
+
+}
+
+type StemAncestorsWalkRequest struct {
+	BasePath string
+	OnMatch  func(context fs2.Walk4Context) error
+	Self bool
+}
+
+func StemAncestorsWalk(args StemAncestorsWalkRequest) error {
+
+	self := args.Self
+	seen := 0
+	onMatch := func(context fs2.Walk4Context) error {
+		// skip the first match if we don't want to see ourselves
+		seen += 1
+		if !self && (seen == 1) {
+			return nil
+		}
+
+		return args.OnMatch(context)
+	}
+
+	return fs2.BFSWalk2(fs2.Walk4Request{
+		Path:        args.BasePath,
+		VPath:       args.BasePath,
+		BasePath:    args.BasePath,
+		ShouldCrawl: StemAncestorsWalkCrawler(),
+		ShouldMatch: StemAncestorsWalkShouldMatch,
+		OnMatch:     onMatch,
+	})
+}
