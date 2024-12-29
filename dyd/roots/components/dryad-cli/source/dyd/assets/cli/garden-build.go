e6557de5d38f9e6c33fac59a3a92253896b2e4bf
James Larsen <somesocks@gmail.com>
2024-12-29 22:22:19 +0100

    - dyd/roots/components/dryad-cli/source - adding basic tasks package - dyd/roots/components/dryad-cli/source - refactoring garden build

--- dyd/roots/components/dryad-cli/source/dyd/assets/cli/garden-build.go
+++ dyd/roots/components/dryad-cli/source/dyd/assets/cli/garden-build.go
@@ -3,11 +3,82 @@ package cli
 import (
 	clib "dryad/cli-builder"
 	dryad "dryad/core"
+	tasks "dryad/tasks"
 
 	zlog "github.com/rs/zerolog/log"
 )
 
 var gardenBuildCommand = func() clib.Command {
+
+	type ParsedArgs struct {
+		IncludeRoots func(path string) bool
+		ExcludeRoots func(path string) bool
+		Path string
+	}
+
+	var parseArgs = func(req clib.ActionRequest) (error, ParsedArgs) {
+		// var args = req.Args
+		var options = req.Opts
+
+		var path string
+		// var err error
+
+		var includeOpts []string
+		var excludeOpts []string
+
+		if options["exclude"] != nil {
+			excludeOpts = options["exclude"].([]string)
+		}
+
+		if options["include"] != nil {
+			includeOpts = options["include"].([]string)
+		}
+
+		if options["path"] != nil {
+			path = options["path"].(string)
+		}
+
+
+		includeRoots := dryad.RootIncludeMatcher(includeOpts)
+		excludeRoots := dryad.RootExcludeMatcher(excludeOpts)
+
+		return nil, ParsedArgs{
+			IncludeRoots: includeRoots,
+			ExcludeRoots: excludeRoots,
+			Path: path,
+		}
+	}
+
+	var buildGarden = func (args ParsedArgs) (error, any) {
+		err := dryad.GardenBuild(
+			dryad.BuildContext{
+				Fingerprints: map[string]string{},
+			},
+			dryad.GardenBuildRequest{
+				BasePath:     args.Path,
+				IncludeRoots: args.IncludeRoots,
+				ExcludeRoots: args.ExcludeRoots,
+			},
+		)
+
+		return err, nil
+	}
+
+	var action = tasks.Return(
+		tasks.Series2(
+			parseArgs,
+			buildGarden,
+		),
+		func (err error, val any) int {
+			if err != nil {
+				zlog.Fatal().Err(err).Msg("error while building garden")
+				return 1
+			}
+
+			return 0
+		},
+	)
+	
 	command := clib.NewCommand("build", "build selected roots in a garden. alias for `dryad roots build`").
 		WithOption(
 			clib.
@@ -16,49 +87,7 @@ var gardenBuildCommand = func() clib.Command {
 		).
 		WithOption(clib.NewOption("include", "choose which roots are included in the build").WithType(clib.OptionTypeMultiString)).
 		WithOption(clib.NewOption("exclude", "choose which roots are excluded from the build").WithType(clib.OptionTypeMultiString)).
-		WithAction(func(req clib.ActionRequest) int {
-			// var args = req.Args
-			var options = req.Opts
-
-			var path string
-			var err error
-
-			var includeOpts []string
-			var excludeOpts []string
-
-			if options["exclude"] != nil {
-				excludeOpts = options["exclude"].([]string)
-			}
-
-			if options["include"] != nil {
-				includeOpts = options["include"].([]string)
-			}
-
-			if options["path"] != nil {
-				path = options["path"].(string)
-			}
-
-			includeRoots := dryad.RootIncludeMatcher(includeOpts)
-			excludeRoots := dryad.RootExcludeMatcher(excludeOpts)
-
-			err = dryad.GardenBuild(
-				dryad.BuildContext{
-					Fingerprints: map[string]string{},
-				},
-				dryad.GardenBuildRequest{
-					BasePath:     path,
-					IncludeRoots: includeRoots,
-					ExcludeRoots: excludeRoots,
-				},
-			)
-			if err != nil {
-				zlog.Fatal().Err(err).Msg("error while building garden")
-				return 1
-			}
-
-			return 0
-		},
-		)
+		WithAction(action)
 
 	command = ScopedCommand(command)
 	command = LoggingCommand(command)
