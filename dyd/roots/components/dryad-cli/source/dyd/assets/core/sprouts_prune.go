19dc99959eda3a8aac68713d070a53a80484f223
James Larsen <somesocks@gmail.com>
2025-01-15 21:19:09 +0100

    - dyd/roots/components/dryad-cli/source - updating sprouts prune to use new FS walk functions - dyd/roots/components/dryad-cli/tests/garden-prune-02 - fixing bug, cleaning up - dyd/roots/components/dryad-cli/tests/sprouts-prune-02 - adding new test for sprouts prune command

--- dyd/roots/components/dryad-cli/source/dyd/assets/core/sprouts_prune.go
+++ dyd/roots/components/dryad-cli/source/dyd/assets/core/sprouts_prune.go
@@ -5,6 +5,8 @@ import (
 	"os"
 	"path/filepath"
 
+	"dryad/task"
+
 	zlog "github.com/rs/zerolog/log"
 )
 
@@ -12,10 +14,6 @@ func SproutsPrune(path string) error {
 	zlog.Debug().Msg("pruning sprouts")
 
 	sproutsPath, err := SproutsPath(path)
-	zlog.Trace().
-		Str("sproutsPath", sproutsPath).
-		Err(err).
-		Msg("SproutsPrune.sproutsPath")
 	if err != nil {
 		return err
 	}
@@ -34,30 +32,31 @@ func SproutsPrune(path string) error {
 	}
 
 	rootsPath, err := RootsPath(path)
-	zlog.Trace().
-		Str("rootsPath", rootsPath).
-		Err(err).
-		Msg("SproutsPrune.rootsPath")
 	if err != nil {
 		return err
 	}
 
 	// crawl everything that isn't a symlink
-	shouldCrawl := func(context fs2.Walk4Context) (bool, error) {
+	shouldCrawl := func(ctx *task.ExecutionContext, node fs2.Walk5Node) (error, bool) {
+		var shouldCrawl bool = node.Info.Mode()&os.ModeSymlink != os.ModeSymlink
+
 		zlog.Trace().
-			Str("path", context.Path).
+			Str("path", node.Path).
+			Bool("shouldCrawl", shouldCrawl).
 			Msg("SproutsPrune.shouldCrawl")
-		return context.Info.Mode()&os.ModeSymlink != os.ModeSymlink, nil
+		return nil, shouldCrawl
 	}
 
 	// match any path that we should delete
-	shouldMatch := func(context fs2.Walk4Context) (bool, error) {
+	shouldMatch := func(ctx *task.ExecutionContext, node fs2.Walk5Node) (error, bool) {
+
 		zlog.Trace().
-			Str("path", context.Path).
+			Str("path", node.Path).
 			Msg("SproutsPrune.shouldMatch")
-		relPath, err := filepath.Rel(context.BasePath, context.Path)
+
+		relPath, err := filepath.Rel(node.BasePath, node.Path)
 		if err != nil {
-			return false, err
+			return err, false
 		}
 
 		rootsEquivalentPath := filepath.Join(rootsPath, relPath)
@@ -65,51 +64,56 @@ func SproutsPrune(path string) error {
 		// if the roots path does not exist, then we should wipe the sprouts path
 		rootsPathExists, err := fileExists(rootsEquivalentPath)
 		if err != nil {
-			return false, err
+			return  err, false
 		}
 		if !rootsPathExists {
-			return true, nil
+			return nil, true
 		}
 
 		// TODO: fix the edge case where a sprout path is within a root
 		// very unlikely to happen, but possible
-		return false, nil
+		return nil, false
 	}
 
-	onMatch := func(context fs2.Walk4Context) error {
+	onMatch := func(ctx *task.ExecutionContext, node fs2.Walk5Node) (error, any) {
 		zlog.Trace().
-			Str("path", context.Path).
+			Str("path", node.Path).
 			Msg("SproutsPrune.onMatch")
-		parentPath := filepath.Dir(context.Path)
+		parentPath := filepath.Dir(node.Path)
 
 		// set parent to RWX--X--X temporarily
 		err = os.Chmod(parentPath, 0o711)
 		if err != nil {
-			return err
+			return err, nil
 		}
 
-		err = os.Remove(context.Path)
+		err = os.Remove(node.Path)
 		if err != nil {
-			return err
+			return err, nil
 		}
 
 		// set parent back to R-X--X--X temporarily
 		err = os.Chmod(parentPath, 0o511)
 		if err != nil {
-			return err
+			return err, nil
 		}
 
-		return nil
+		return nil, nil
 	}
 
-	err = fs2.DFSWalk2(fs2.Walk4Request{
-		Path:        sproutsPath,
-		VPath:       sproutsPath,
-		BasePath:    sproutsPath,
-		ShouldCrawl: shouldCrawl,
-		ShouldMatch: shouldMatch,
-		OnMatch:     onMatch,
-	})
+	// NOTE: this needs to run serially until we fix the concurrency issue
+	// with parent permissions in onMatch
+	err = fs2.DFSWalk3(
+		task.SERIAL_CONTEXT,
+		fs2.Walk5Request{
+			Path:        sproutsPath,
+			VPath:       sproutsPath,
+			BasePath:    sproutsPath,
+			ShouldCrawl: shouldCrawl,
+			ShouldMatch: shouldMatch,
+			OnMatch:     onMatch,
+		},
+	)
 	if err != nil {
 		return err
 	}
---
+++ dyd/roots/components/dryad-cli/tests/sprouts-prune-02/dyd/docs/about.md
@@ -0,0 +1,4 @@
+
+# sprouts-prune-02
+
+This test case tests that `dryad sprouts prune` runs successfully against a garden that has a root and built sprout.
