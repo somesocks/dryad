71a2c01458aab1dde317ba0f53004ef2517eb682
James Larsen <somesocks@gmail.com>
2025-01-22 19:18:09 +0100

    - dyd/roots/components/dryad-cli/source - updating RootsWalk to use concurrency

--- dyd/roots/components/dryad-cli/source/dyd/assets/core/roots_walk.go
+++ dyd/roots/components/dryad-cli/source/dyd/assets/core/roots_walk.go
@@ -2,36 +2,85 @@ package core
 
 import (
 	fs2 "dryad/filesystem"
-	"io/fs"
+	"dryad/task"
 	"os"
 	"path/filepath"
 )
 
-var _isRoot = func(path string, info fs.FileInfo) (bool, error) {
-
-	typePath := filepath.Join(path, "dyd", "type")
-	typeBytes, err := os.ReadFile(typePath)
-
-	isRoot := err == nil && string(typeBytes) == "root"
-
-	return isRoot, nil
+type RootsWalkRequest struct {
+	GardenPath string
+	OnRoot func (ctx *task.ExecutionContext, match RootsWalkMatch) (error, any)
 }
 
-func RootsWalk(path string, walkFn func(path string, info fs.FileInfo) error) error {
-	var rootsPath, err = RootsPath(path)
-	if err != nil {
-		return err
-	}
-
-	err = fs2.Walk(fs2.WalkRequest{
-		BasePath:     rootsPath,
-		CrawlExclude: _isRoot,
-		MatchInclude: _isRoot,
-		OnMatch:      walkFn,
-	})
-	if err != nil {
-		return err
-	}
-
-	return nil
+type RootsWalkMatch struct {
+	RootPath string
+	GardenPath string
 }
+
+func RootsWalk(ctx *task.ExecutionContext, req RootsWalkRequest) (error, any) {
+	var rootsPath, err = RootsPath(req.GardenPath)
+	if err != nil {
+		return err, nil
+	}
+
+	var isRoot = func(ctx *task.ExecutionContext, node fs2.Walk5Node) (error, bool) {
+		typePath := filepath.Join(node.Path, "dyd", "type")
+		typeBytes, err := os.ReadFile(typePath)
+	
+		isRoot := err == nil && string(typeBytes) == "root"
+	
+		return  nil, isRoot
+	}
+	
+	var shouldCrawl = func(ctx *task.ExecutionContext, node fs2.Walk5Node) (error, bool) {
+		err, isRoot := isRoot(ctx, node)
+		return err, !isRoot
+	}
+	
+	var shouldMatch = func(ctx *task.ExecutionContext, node fs2.Walk5Node) (error, bool) {
+		err, isRoot := isRoot(ctx, node)
+		return err, isRoot
+	}
+
+	var onMatch = func(ctx *task.ExecutionContext, node fs2.Walk5Node) (error, any) {
+		err, _ := req.OnRoot(ctx, RootsWalkMatch{
+			GardenPath: req.GardenPath,
+			RootPath: node.Path,
+		})
+		return err, nil
+	}
+
+	err, _ = fs2.BFSWalk3(
+		ctx,
+		fs2.Walk5Request{
+			Path: rootsPath,
+			VPath: rootsPath,
+			BasePath: rootsPath,
+			ShouldCrawl: shouldCrawl,
+			ShouldMatch: shouldMatch,
+			OnMatch: onMatch,
+		},
+	)
+
+	return err, nil
+}
+	
+	
+// 	path string, walkFn func(path string, info fs.FileInfo) error) error {
+// 	var rootsPath, err = RootsPath(path)
+// 	if err != nil {
+// 		return err
+// 	}
+
+// 	err = fs2.Walk(fs2.WalkRequest{
+// 		BasePath:     rootsPath,
+// 		CrawlExclude: _isRoot,
+// 		MatchInclude: _isRoot,
+// 		OnMatch:      walkFn,
+// 	})
+// 	if err != nil {
+// 		return err
+// 	}
+
+// 	return nil
+// }
