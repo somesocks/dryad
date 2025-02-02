743bfaa90b562121a2a5e905b0f00eef7543f9d1
James Larsen <somesocks@gmail.com>
2025-02-02 12:18:52 +0100

    - dyd/roots/components/dryad-cli/source - updating root build cli command to implement concurrency

--- dyd/roots/components/dryad-cli/source/dyd/assets/cli/root-build.go
+++ dyd/roots/components/dryad-cli/source/dyd/assets/cli/root-build.go
@@ -12,18 +12,20 @@ import (
 )
 
 var rootBuildCommand = func() clib.Command {
-	command := clib.
-		NewCommand("build", "build a specified root").
-		WithArg(
-			clib.
-				NewArg("path", "path to the root to build").
-				AsOptional().
-				WithAutoComplete(ArgAutoCompletePath),
-		).
-		WithAction(func(req clib.ActionRequest) int {
+
+	type ParsedArgs struct {
+		RootPath string
+		Parallel int
+	}
+
+	var parseArgs = task.From(
+		func(req clib.ActionRequest) (error, ParsedArgs) {
 			var args = req.Args
+			var options = req.Opts
 
 			var path string
+
+			var parallel int
 
 			if len(args) > 0 {
 				path = args[0]
@@ -32,30 +34,74 @@ var rootBuildCommand = func() clib.Command {
 			if !filepath.IsAbs(path) {
 				wd, err := os.Getwd()
 				if err != nil {
-					zlog.Fatal().Err(err).Msg("error while finding working directory")
-					return 1
+					return err, ParsedArgs{}
 				}
 				path = filepath.Join(wd, path)
 			}
 
-			var rootFingerprint string
-			err, rootFingerprint := dryad.RootBuild(
-				task.SERIAL_CONTEXT,
-				dryad.RootBuildRequest{
-					RootPath: path,	
-				},
-			)
+			if options["parallel"] != nil {
+				parallel = int(options["parallel"].(int64))
+			} else {
+				parallel = 8
+			}
+				
+			return nil, ParsedArgs{
+				RootPath: path,
+				Parallel: parallel,
+			}
+		},
+	)
+
+	var buildRoot = func (ctx *task.ExecutionContext, args ParsedArgs) (error, any) {
+		var rootFingerprint string
+		err, rootFingerprint := dryad.RootBuild(
+			task.SERIAL_CONTEXT,
+			dryad.RootBuildRequest{
+				RootPath: args.RootPath,	
+			},
+		)
+		if err != nil {
+			return err, nil
+		}
+		fmt.Println(rootFingerprint)
+
+		return nil, nil
+	}
+
+	buildRoot = task.WithContext(
+		buildRoot,
+		func (ctx *task.ExecutionContext, args ParsedArgs) (error, *task.ExecutionContext) {
+			return nil, task.NewContext(args.Parallel)
+		},
+	)
+
+	var action = task.Return(
+		task.Series2(
+			parseArgs,
+			buildRoot,
+		),
+		func (err error, val any) int {
 			if err != nil {
 				zlog.Fatal().Err(err).Msg("error while building root")
 				return 1
 			}
-			fmt.Println(rootFingerprint)
 
 			return 0
-		})
+		},
+	)
 
+	command := clib.
+		NewCommand("build", "build a specified root").
+		WithArg(
+			clib.
+				NewArg("path", "path to the root to build").
+				AsOptional().
+				WithAutoComplete(ArgAutoCompletePath),
+		).
+		WithAction(action)
+
+	command = ParallelCommand(command)
 	command = LoggingCommand(command)
-
 
 	return command
 }()
