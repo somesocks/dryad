cf767fc89d2bf75322c48c2479ad9a1b963c0d02
James Larsen <somesocks@gmail.com>
2025-02-09 20:33:06 +0100

    - dyd/roots/components/dryad-cli/source - making GardenPath private

--- dyd/roots/components/dryad-cli/source/dyd/assets/core/garden_unsafe_resolve.go
+++ dyd/roots/components/dryad-cli/source/dyd/assets/core/garden_unsafe_resolve.go
@@ -4,9 +4,53 @@ package core
 import (
 	"dryad/task"
 
+	"path/filepath"
+	"os"
+	"errors"
+
 	zlog "github.com/rs/zerolog/log"
 )
 
+func _gardenPath(path string) (string, error) {
+	zlog.Trace().
+		Str("path", path).
+		Msg("GardenPath")
+
+	var err error
+	path, err = filepath.Abs(path)
+	if err != nil {
+		return "", err
+	}
+
+	zlog.Trace().
+		Str("path", path).
+		Msg("GardenPath/abs")
+
+	path, err = filepath.EvalSymlinks(path)
+	if err != nil {
+		return "", err
+	}
+	zlog.Trace().
+		Str("path", path).
+		Msg("GardenPath/evalSym")
+
+	var workingPath = path
+	var flagPath = filepath.Join(workingPath, "dyd", "type")
+	var fileBytes, fileInfoErr = os.ReadFile(flagPath)
+
+	for workingPath != "/" {
+
+		if fileInfoErr == nil && string(fileBytes) == "garden" {
+			return workingPath, nil
+		}
+
+		workingPath = filepath.Dir(workingPath)
+		flagPath = filepath.Join(workingPath, "dyd", "type")
+		fileBytes, fileInfoErr = os.ReadFile(flagPath)
+	}
+
+	return "", errors.New("dyd garden path not found starting from " + path)
+}
 
 func (ug *UnsafeGardenReference) Resolve(ctx * task.ExecutionContext, _ any) (error, SafeGardenReference) {
 	var gardenPath string = ug.BasePath
@@ -16,6 +60,6 @@ func (ug *UnsafeGardenReference) Resolve(ctx * task.ExecutionContext, _ any) (er
 		Str("BasePath", ug.BasePath).
 		Msg("UnsafeGardenReference.Resolve")
 
-	gardenPath, err = GardenPath(ug.BasePath)
+	gardenPath, err = _gardenPath(ug.BasePath)
 	return err, SafeGardenReference{ BasePath: gardenPath }
 }
