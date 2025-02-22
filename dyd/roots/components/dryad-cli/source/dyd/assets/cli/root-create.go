45b542d1c3cbc6846c2d854da756832d51c9d6d5
James Larsen <somesocks@gmail.com>
2025-02-22 22:45:02 +0100

    - dyd/roots/components/dryad-cli/source - refactoring `root create` command, adding --parallel option

--- dyd/roots/components/dryad-cli/source/dyd/assets/cli/root-create.go
+++ dyd/roots/components/dryad-cli/source/dyd/assets/cli/root-create.go
@@ -6,54 +6,108 @@ import (
 	"dryad/task"
 	"fmt"
 
+	"path/filepath"
+
 	zlog "github.com/rs/zerolog/log"
 )
 
 var rootCreateCommand = func() clib.Command {
+
+	type ParsedArgs struct {
+		RootPath string
+		Parallel int
+	}
+
+	var parseArgs task.Task[clib.ActionRequest, ParsedArgs] = 
+		func(ctx *task.ExecutionContext, req clib.ActionRequest) (error, ParsedArgs) {
+			var args = req.Args
+			var options = req.Opts
+			var err error 
+
+			var rootPath string
+
+			if len(args) > 0 {
+				rootPath = args[0]
+			}
+
+			var parallel int
+
+			if options["parallel"] != nil {
+				parallel = int(options["parallel"].(int64))
+			} else {
+				parallel = 8
+			}
+
+			rootPath, err = filepath.Abs(rootPath)
+			if err != nil {
+				return err, ParsedArgs{}
+			}
+	
+			return nil, ParsedArgs{
+				RootPath: rootPath,
+				Parallel: parallel,
+			}
+		}
+
+	var createRoot task.Task[ParsedArgs, any] =
+		func (ctx *task.ExecutionContext, args ParsedArgs) (error, any) {
+
+			err, garden := dryad.Garden("").Resolve(ctx)
+			if err != nil {
+				return err, nil
+			}
+
+			err, roots := garden.Roots().Resolve(ctx)
+			if err != nil {
+				return err, nil
+			}
+
+			err, unsafeRoot := roots.Root(args.RootPath).Clean()
+			if err != nil {
+				return err, nil
+			}
+	
+			err, safeRoot := unsafeRoot.Create(
+				ctx,
+			)
+
+			fmt.Println(safeRoot.BasePath)
+
+			return nil, nil
+		}
+
+	createRoot = task.WithContext(
+		createRoot,
+		func (ctx *task.ExecutionContext, args ParsedArgs) (error, *task.ExecutionContext) {
+			return nil, task.NewContext(args.Parallel)
+		},
+	)
+
+	var action = task.Return(
+		task.Series2(
+			parseArgs,
+			createRoot,
+		),
+		func (err error, val any) int {
+			if err != nil {
+				zlog.Fatal().Err(err).Msg("error while creating root")
+				return 1
+			}
+
+			return 0
+		},
+	)
+
 	command := clib.NewCommand("create", "create a new root at the target path").
 		WithArg(
 			clib.
 				NewArg("path", "the path to create the new root at").
 				WithAutoComplete(ArgAutoCompletePath),
 		).
-		WithAction(func(req clib.ActionRequest) int {
-			var args = req.Args
+		WithAction(action)
 
-			var path string = args[0]
-			
-			err, garden := dryad.Garden("").Resolve(task.SERIAL_CONTEXT)
-			if err != nil {
-				zlog.Fatal().Err(err).Msg("error resolving garden")
-				return 1
-			}
-
-			err, roots := garden.Roots().Resolve(task.SERIAL_CONTEXT)
-			if err != nil {
-				zlog.Fatal().Err(err).Msg("error resolving garden roots")
-				return 1
-			}
-
-			err, unsafeRoot := roots.Root(path).Clean()
-			if err != nil {
-				zlog.Fatal().Err(err).Msg("error resolving destination root location")
-				return 1
-			}
-	
-			err, safeRoot := unsafeRoot.Create(
-				task.SERIAL_CONTEXT,
-			)
-
-			if err != nil {
-				zlog.Fatal().Err(err).Msg("error while creating root")
-				return 1
-			}
-
-			fmt.Println(safeRoot.BasePath)
-			return 0
-		})
-
+	command = ParallelCommand(command)
 	command = LoggingCommand(command)
-
 
 	return command
 }()
