48125b72680e8d137a99cd4f4a6fb34637075bdd
James Larsen <somesocks@gmail.com>
2025-02-27 23:53:48 +0100

    - dyd/roots/components/dryad-cli/source - adding new `dryad roots each` command

---
+++ dyd/roots/components/dryad-cli/source/dyd/assets/cli/roots-each.go
@@ -0,0 +1,255 @@
+package cli
+
+import (
+	clib "dryad/cli-builder"
+	dryad "dryad/core"
+	"dryad/task"
+	// "fmt"
+	// "path/filepath"
+
+	"os"
+	"os/exec"
+
+	"strings"
+
+
+	zlog "github.com/rs/zerolog/log"
+)
+
+var rootsEachCommand = func() clib.Command {
+
+	type ParsedArgs struct {
+		Include []string
+		Exclude []string
+		Shell string
+		Command string
+		GardenPath string
+		Parallel int
+		JoinStdout bool
+		JoinStderr bool
+		IgnoreErrors bool
+	}
+
+	var parseArgs = task.From(
+		func(req clib.ActionRequest) (error, ParsedArgs) {
+			var args = req.Args
+			var options = req.Opts
+
+			var path string = ""
+
+			var joinStdout bool
+			var joinStderr bool
+	
+			var ignoreErrors bool
+
+			var includeOpts []string
+			var excludeOpts []string
+
+			if options["exclude"] != nil {
+				excludeOpts = options["exclude"].([]string)
+			}
+
+			if options["include"] != nil {
+				includeOpts = options["include"].([]string)
+			}
+
+			var parallel int
+
+			if options["parallel"] != nil {
+				parallel = int(options["parallel"].(int64))
+			} else {
+				parallel = 8
+			}
+	
+			var shell string
+			if options["shell"] != nil {
+				includeOpts = options["shell"].([]string)
+			}
+			if shell == "" {
+				shell = os.Getenv("SHELL")
+			}
+			if shell == "" {
+				shell = "/bin/sh"
+			}
+
+			if options["join-stdout"] != nil {
+				joinStdout = options["join-stdout"].(bool)
+			} else {
+				joinStdout = false
+			}
+	
+			if options["join-stderr"] != nil {
+				joinStderr = options["join-stderr"].(bool)
+			} else {
+				joinStderr = false
+			}
+
+			if options["ignore-errors"] != nil {
+				ignoreErrors = options["ignore-errors"].(bool)
+			} else {
+				ignoreErrors = false
+			}
+
+			var command string = strings.Join(args[:], " ")
+
+			return nil, ParsedArgs{
+				GardenPath: path,
+				Parallel: parallel,
+				Shell: shell,
+				Command: command,
+				Include: includeOpts,
+				Exclude: excludeOpts,
+				JoinStdout: joinStdout,
+				JoinStderr: joinStderr,
+				IgnoreErrors: ignoreErrors,
+			}
+		},
+	)
+
+	var eachRoots = func (ctx *task.ExecutionContext, args ParsedArgs) (error, any) {
+		unsafeGarden := dryad.Garden(args.GardenPath)
+		
+		err, garden := unsafeGarden.Resolve(ctx)
+		if err != nil {
+			return err, nil
+		}
+
+		err, roots := garden.Roots().Resolve(task.SERIAL_CONTEXT)
+		if err != nil {
+			return err, nil
+		}
+
+		err = roots.Walk(
+			ctx,
+			dryad.RootsWalkRequest{
+				OnMatch: func (ctx *task.ExecutionContext, root *dryad.SafeRootReference) (error, any) {
+					var err error
+					var matchesInclude = false
+					var matchesExclude = false
+
+					if len(args.Include) == 0 { matchesInclude = true }
+
+					for _, include := range args.Include {
+						var matchesFilter bool
+						err, matchesFilter = root.Filter(
+							ctx,
+							dryad.RootFilterRequest{
+								Expression: include,
+							},
+						)
+						if err != nil {
+							return err, nil
+						}
+						matchesInclude = matchesInclude || matchesFilter
+						if matchesInclude {
+							break
+						}
+					}
+
+					for _, exclude := range args.Exclude {
+						var matchesFilter bool
+						err, matchesFilter = root.Filter(
+							ctx,
+							dryad.RootFilterRequest{
+								Expression: exclude,
+							},
+						)
+						if err != nil {
+							return err, nil
+						}
+						matchesExclude = matchesExclude || matchesFilter
+						if matchesExclude {
+							break
+						}
+					}
+
+					if matchesInclude && !matchesExclude {
+
+						cmd := exec.Command(
+							args.Shell,
+							[]string{"-c", args.Command}...,
+						)
+					
+						cmd.Dir = root.BasePath
+
+						cmd.Stdin = os.Stdin
+
+						// optionally pipe the exec logs to us
+						if args.JoinStdout {
+							cmd.Stdout = os.Stdout
+						}
+					
+						// optionally pipe the exec stderr to us
+						if args.JoinStderr {
+							cmd.Stderr = os.Stderr
+						}
+											
+						err = cmd.Run()
+						if err != nil && !args.IgnoreErrors {
+							return err, nil
+						}
+					}
+
+					return nil, nil
+				},
+			},
+		)
+		return err, nil
+	}
+
+	eachRoots = task.WithContext(
+		eachRoots,
+		func (ctx *task.ExecutionContext, args ParsedArgs) (error, *task.ExecutionContext) {
+			return nil, task.NewContext(args.Parallel)
+		},
+	)
+
+
+	var action = task.Return(
+		task.Series2(
+			parseArgs,
+			eachRoots,
+		),
+		func (err error, val any) int {
+			if err != nil {
+				zlog.Fatal().Err(err).Msg("error while executing commands")
+				return 1
+			}
+
+			return 0
+		},
+	)
+
+	command := clib.NewCommand("each", "run a command once for each root. the current working directory is set to the base path of the root for each execution.").
+		WithOption(clib.NewOption("include", "choose which roots are included in the list. the include filter is a CEL expression with access to a 'root' object that can be used to filter on properties of the root.").WithType(clib.OptionTypeMultiString)).
+		WithOption(clib.NewOption("exclude", "choose which roots are excluded from the list.  the exclude filter is a CEL expression with access to a 'root' object that can be used to filter on properties of the root.").WithType(clib.OptionTypeMultiString)).
+		WithOption(
+			clib.NewOption(
+				"ignore-errors",
+				"continue running even if one command execution returns an error",
+			).
+			WithType(clib.OptionTypeBool),
+		).
+		WithOption(
+			clib.NewOption(
+				"join-stdout",
+				"join the stdout of child processes to the stderr of the parent dryad process. default false",
+			).
+			WithType(clib.OptionTypeBool),
+		).
+		WithOption(
+			clib.NewOption(
+				"join-stderr",
+				"join the stderr of child processes to the stderr of the parent dryad process. default false",
+			).
+			WithType(clib.OptionTypeBool),
+		).
+		WithArg(clib.NewArg("-- command", "command to run once for each root").AsOptional()).
+		WithAction(action)
+
+	command = ParallelCommand(command)
+	command = ScopedCommand(command)
+	command = LoggingCommand(command)
+
+	return command
+}()
--- dyd/roots/components/dryad-cli/source/dyd/assets/cli/roots.go
+++ dyd/roots/components/dryad-cli/source/dyd/assets/cli/roots.go
@@ -7,6 +7,7 @@ import (
 var rootsCommand = clib.NewCommand("roots", "commands to work with dryad roots").
 	WithCommand(rootsAffectedCommand).
 	WithCommand(rootsBuildCommand).
+	WithCommand(rootsEachCommand).
 	WithCommand(rootsGraphCommand).
 	WithCommand(rootsListCommand).
 	WithCommand(rootsOwningCommand).
