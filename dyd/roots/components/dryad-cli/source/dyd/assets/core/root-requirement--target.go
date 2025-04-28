e8d0dfb6163a41bdfbab40f4e4594b8deb5a606c
James Larsen <somesocks@gmail.com>
2025-04-28 22:48:15 +0200

    - dyd/roots/components/dryad-cli/source - adding compatibility check for new requirement format

--- dyd/roots/components/dryad-cli/source/dyd/assets/core/root-requirement--target.go
+++ dyd/roots/components/dryad-cli/source/dyd/assets/core/root-requirement--target.go
@@ -8,7 +8,9 @@ import (
 	"os"
 	"path/filepath"
 
-	// "errors"
+	"net/url"
+
+	"fmt"
 	// zlog "github.com/rs/zerolog/log"
 )
 
@@ -17,23 +19,66 @@ func (rootRequirement *SafeRootRequirementReference) Target(ctx * task.Execution
 	var safeRef SafeRootReference
 
 
-	linkPath, err := os.Readlink(rootRequirement.BasePath)
+	linkInfo, err := os.Lstat(rootRequirement.BasePath)
 	if err != nil {
 		return err, nil
 	}
 
-	// convert relative links to an absolute path
-	if !filepath.IsAbs(linkPath) {
+	isSymlink := linkInfo.Mode()&os.ModeSymlink == os.ModeSymlink
+
+	// compatibility with legacy gardens - if a requirement is a symlink
+	// resolve it directly
+	if isSymlink {
+		linkPath, err := os.Readlink(rootRequirement.BasePath)
+
+		// convert relative links to an absolute path
+		if !filepath.IsAbs(linkPath) {
+			linkPath = filepath.Join(
+				filepath.Dir(rootRequirement.BasePath),
+				linkPath,
+			)
+		}
+
+		err, safeRef = rootRequirement.Requirements.Root.Roots.Root(linkPath).Resolve(ctx)
+		if err != nil {
+			return err, nil
+		}
+
+		return nil, &safeRef
+	} else {
+		linkBytes, err := os.ReadFile(rootRequirement.BasePath)
+		if err != nil {
+			return err, nil
+		}
+
+		linkString := string(linkBytes)
+		linkUrl, err := url.Parse(linkString)
+		if err != nil {
+			return err, nil
+		}
+
+		linkScheme := linkUrl.Scheme
+
+		if linkScheme != "root" {
+			return fmt.Errorf("unsupported scheme for root requirement: %s", linkScheme), nil
+		}
+
+		linkPath := linkUrl.Path
+		if filepath.IsAbs(linkPath) {
+			return fmt.Errorf("root requirement path must be relative", linkScheme), nil
+		}
+
 		linkPath = filepath.Join(
 			filepath.Dir(rootRequirement.BasePath),
 			linkPath,
 		)
+
+		err, safeRef = rootRequirement.Requirements.Root.Roots.Root(linkPath).Resolve(ctx)
+		if err != nil {
+			return err, nil
+		}
+
+		return nil, &safeRef
 	}
 
-	err, safeRef = rootRequirement.Requirements.Root.Roots.Root(linkPath).Resolve(ctx)
-	if err != nil {
-		return err, nil
-	}
-
-	return nil, &safeRef 
 }
