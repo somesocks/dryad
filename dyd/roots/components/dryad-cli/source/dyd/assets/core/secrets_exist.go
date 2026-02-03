21bdf711bb0b0f1022d560553ad53c6e8b469ccb
James L <somesocks@gmail.com>
2026-02-03 22:02:23 +0100

    - dyd/roots/components/dryad-cli/source - simplifying secrets exist code

--- dyd/roots/components/dryad-cli/source/dyd/assets/core/secrets_exist.go
+++ dyd/roots/components/dryad-cli/source/dyd/assets/core/secrets_exist.go
@@ -1,41 +1,9 @@
 package core
 
-import "io/fs"
-
 func SecretsExist(path string) (bool, error) {
-	var err error
-	var exists bool
-	var found bool
-
-	path, err = SecretsPath(path)
+	secretsPath, err := SecretsPath(path)
 	if err != nil {
 		return false, err
 	}
-
-	// return false if the folder doesn't exist
-	exists, err = fileExists(path)
-	if err != nil {
-		return false, err
-	}
-	if !exists {
-		return false, nil
-	}
-
-	err = SecretsWalk(
-		SecretsWalkArgs{
-			BasePath: path,
-			OnMatch: func(_ string, info fs.FileInfo) error {
-				if info.IsDir() {
-					return nil
-				}
-				found = true
-				return nil
-			},
-		},
-	)
-	if err != nil {
-		return false, err
-	}
-
-	return found, nil
+	return fileExists(secretsPath)
 }
