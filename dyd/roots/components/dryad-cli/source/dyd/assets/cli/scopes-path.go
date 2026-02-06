a4824f5105f3187071ae3981a7edd05917684b4b
James L <somesocks@gmail.com>
2026-02-06 17:37:03 +0100

    - dyd/roots/components/dryad-cli/source - refactoring scopes path command to task pipeline with --parallel context support

--- dyd/roots/components/dryad-cli/source/dyd/assets/cli/scopes-path.go
+++ dyd/roots/components/dryad-cli/source/dyd/assets/cli/scopes-path.go
@@ -11,34 +11,78 @@ import (
 )
 
 var scopesPathCommand = func() clib.Command {
-	command := clib.NewCommand("path", "return the path of the scopes dir").
-		WithAction(func(req clib.ActionRequest) int {
+	type ParsedArgs struct {
+		GardenPath string
+		Parallel   int
+	}
+
+	var parseArgs = task.From(
+		func(req clib.ActionRequest) (error, ParsedArgs) {
+			var opts = req.Opts
+			var parallel int
+
+			if opts["parallel"] != nil {
+				parallel = int(opts["parallel"].(int64))
+			} else {
+				parallel = PARALLEL_COUNT_DEFAULT
+			}
+
 			var path, err = os.Getwd()
 			if err != nil {
-				zlog.Fatal().Err(err).Msg("error while finding working directory")
-				return 1
+				return err, ParsedArgs{}
 			}
 
-			unsafeGarden := dryad.Garden(path)
-			
-			err, garden := unsafeGarden.Resolve(task.SERIAL_CONTEXT)
-			if err != nil {
-				return 1
+			return nil, ParsedArgs{
+				GardenPath: path,
+				Parallel:   parallel,
 			}
+		},
+	)
 
-			path, err = dryad.ScopesPath(garden)
+	var getScopesPath = func(ctx *task.ExecutionContext, args ParsedArgs) (error, any) {
+		unsafeGarden := dryad.Garden(args.GardenPath)
 
+		err, garden := unsafeGarden.Resolve(ctx)
+		if err != nil {
+			return err, nil
+		}
+
+		path, err := dryad.ScopesPath(garden)
+		if err != nil {
+			return err, nil
+		}
+
+		fmt.Println(path)
+
+		return nil, nil
+	}
+
+	getScopesPath = task.WithContext(
+		getScopesPath,
+		func(ctx *task.ExecutionContext, args ParsedArgs) (error, *task.ExecutionContext) {
+			return nil, task.NewContext(args.Parallel)
+		},
+	)
+
+	var action = task.Return(
+		task.Series2(
+			parseArgs,
+			getScopesPath,
+		),
+		func(err error, val any) int {
 			if err != nil {
 				zlog.Fatal().Err(err).Msg("error while finding scopes path")
 				return 1
 			}
-			fmt.Println(path)
-
 			return 0
-		})
+		},
+	)
 
+	command := clib.NewCommand("path", "return the path of the scopes dir").
+		WithAction(action)
+
+	command = ParallelCommand(command)
 	command = LoggingCommand(command)
-
 
 	return command
 }()
