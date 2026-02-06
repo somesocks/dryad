1313c5076188da0eb067cf136a06c4db4188d9dd
James L <somesocks@gmail.com>
2026-02-06 17:10:51 +0100

    - dyd/roots/components/dryad-cli/source - refactoring scope setting set command to task pipeline with --parallel context support

--- dyd/roots/components/dryad-cli/source/dyd/assets/cli/scope-setting-set.go
+++ dyd/roots/components/dryad-cli/source/dyd/assets/cli/scope-setting-set.go
@@ -10,6 +10,82 @@ import (
 )
 
 var scopeSettingSetCommand = func() clib.Command {
+	type ParsedArgs struct {
+		Scope      string
+		Setting    string
+		Value      string
+		GardenPath string
+		Parallel   int
+	}
+
+	var parseArgs = task.From(
+		func(req clib.ActionRequest) (error, ParsedArgs) {
+			var args = req.Args
+			var opts = req.Opts
+
+			var scope string = args[0]
+			var setting string = args[1]
+			var value string = args[2]
+			var parallel int
+
+			if opts["parallel"] != nil {
+				parallel = int(opts["parallel"].(int64))
+			} else {
+				parallel = PARALLEL_COUNT_DEFAULT
+			}
+
+			var path, err = os.Getwd()
+			if err != nil {
+				return err, ParsedArgs{}
+			}
+
+			return nil, ParsedArgs{
+				Scope:      scope,
+				Setting:    setting,
+				Value:      value,
+				GardenPath: path,
+				Parallel:   parallel,
+			}
+		},
+	)
+
+	var setScopeSetting = func(ctx *task.ExecutionContext, args ParsedArgs) (error, any) {
+		unsafeGarden := dryad.Garden(args.GardenPath)
+
+		err, garden := unsafeGarden.Resolve(ctx)
+		if err != nil {
+			return err, nil
+		}
+
+		err = dryad.ScopeSettingSet(garden, args.Scope, args.Setting, args.Value)
+		if err != nil {
+			return err, nil
+		}
+
+		return nil, nil
+	}
+
+	setScopeSetting = task.WithContext(
+		setScopeSetting,
+		func(ctx *task.ExecutionContext, args ParsedArgs) (error, *task.ExecutionContext) {
+			return nil, task.NewContext(args.Parallel)
+		},
+	)
+
+	var action = task.Return(
+		task.Series2(
+			parseArgs,
+			setScopeSetting,
+		),
+		func(err error, val any) int {
+			if err != nil {
+				zlog.Fatal().Err(err).Msg("error while changing scope setting")
+				return 1
+			}
+			return 0
+		},
+	)
+
 	command := clib.NewCommand("set", "set the value of a setting in a scope").
 		WithArg(
 			clib.
@@ -18,37 +94,10 @@ var scopeSettingSetCommand = func() clib.Command {
 		).
 		WithArg(clib.NewArg("setting", "the name of the setting")).
 		WithArg(clib.NewArg("value", "the new value for the setting")).
-		WithAction(func(req clib.ActionRequest) int {
-			var args = req.Args
+		WithAction(action)
 
-			var scope string = args[0]
-			var setting string = args[1]
-			var value string = args[2]
-
-			var path, err = os.Getwd()
-			if err != nil {
-				zlog.Fatal().Err(err).Msg("error while finding working directory")
-				return 1
-			}
-
-			unsafeGarden := dryad.Garden(path)
-			
-			err, garden := unsafeGarden.Resolve(task.SERIAL_CONTEXT)
-			if err != nil {
-				return 1
-			}
-
-			err = dryad.ScopeSettingSet(garden, scope, setting, value)
-			if err != nil {
-				zlog.Fatal().Err(err).Msg("error while changing scope setting")
-				return 1
-			}
-
-			return 0
-		})
-
+	command = ParallelCommand(command)
 	command = LoggingCommand(command)
-
 
 	return command
 }()
