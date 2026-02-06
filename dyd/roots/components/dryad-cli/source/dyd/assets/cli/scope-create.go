f8483e86670172fcce346703746c69edfdc3fdad
James L <somesocks@gmail.com>
2026-02-06 16:56:23 +0100

    - dyd/roots/components/dryad-cli/source - refactoring scope create command to task pipeline with --parallel context support

--- dyd/roots/components/dryad-cli/source/dyd/assets/cli/scope-create.go
+++ dyd/roots/components/dryad-cli/source/dyd/assets/cli/scope-create.go
@@ -11,39 +11,84 @@ import (
 )
 
 var scopeCreateCommand = func() clib.Command {
-	command := clib.NewCommand("create", "create a new scope directory for the garden").
-		WithArg(clib.NewArg("name", "the name of the new scope")).
-		WithAction(func(req clib.ActionRequest) int {
+	type ParsedArgs struct {
+		Name       string
+		GardenPath string
+		Parallel   int
+	}
+
+	var parseArgs = task.From(
+		func(req clib.ActionRequest) (error, ParsedArgs) {
 			var args = req.Args
+			var opts = req.Opts
 
 			var name string = args[0]
+			var parallel int
+
+			if opts["parallel"] != nil {
+				parallel = int(opts["parallel"].(int64))
+			} else {
+				parallel = PARALLEL_COUNT_DEFAULT
+			}
 
 			var path, err = os.Getwd()
 			if err != nil {
-				zlog.Fatal().Err(err).Msg("error while finding working directory")
-				return 1
+				return err, ParsedArgs{}
 			}
 
-			unsafeGarden := dryad.Garden(path)
-			
-			err, garden := unsafeGarden.Resolve(task.SERIAL_CONTEXT)
-			if err != nil {
-				return 1
+			return nil, ParsedArgs{
+				Name:       name,
+				GardenPath: path,
+				Parallel:   parallel,
 			}
+		},
+	)
 
-			scopePath, err := dryad.ScopeCreate(garden, name)
+	var createScope = func(ctx *task.ExecutionContext, args ParsedArgs) (error, any) {
+		unsafeGarden := dryad.Garden(args.GardenPath)
+
+		err, garden := unsafeGarden.Resolve(ctx)
+		if err != nil {
+			return err, nil
+		}
+
+		scopePath, err := dryad.ScopeCreate(garden, args.Name)
+		if err != nil {
+			return err, nil
+		}
+
+		fmt.Println(scopePath)
+
+		return nil, nil
+	}
+
+	createScope = task.WithContext(
+		createScope,
+		func(ctx *task.ExecutionContext, args ParsedArgs) (error, *task.ExecutionContext) {
+			return nil, task.NewContext(args.Parallel)
+		},
+	)
+
+	var action = task.Return(
+		task.Series2(
+			parseArgs,
+			createScope,
+		),
+		func(err error, val any) int {
 			if err != nil {
 				zlog.Fatal().Err(err).Msg("error while creating scope")
 				return 1
 			}
-
-			fmt.Println(scopePath)
-
 			return 0
-		})
+		},
+	)
 
+	command := clib.NewCommand("create", "create a new scope directory for the garden").
+		WithArg(clib.NewArg("name", "the name of the new scope")).
+		WithAction(action)
+
+	command = ParallelCommand(command)
 	command = LoggingCommand(command)
-
 
 	return command
 }()
