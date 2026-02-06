9c5fd60f98a99e75555798783fb4708074ff5752
James L <somesocks@gmail.com>
2026-02-06 19:40:42 +0100

    refactor(cli): parallelize stem path

--- dyd/roots/components/dryad-cli/source/dyd/assets/cli/stem-path.go
+++ dyd/roots/components/dryad-cli/source/dyd/assets/cli/stem-path.go
@@ -3,6 +3,7 @@ package cli
 import (
 	clib "dryad/cli-builder"
 	dryad "dryad/core"
+	"dryad/task"
 	"fmt"
 	"os"
 
@@ -10,26 +11,67 @@ import (
 )
 
 var stemPathCommand = func() clib.Command {
-	command := clib.NewCommand("path", "return the base path of the current root").
-		// WithArg(clib.NewArg("path", "path to the stem base dir")).
-		WithAction(func(req clib.ActionRequest) int {
-			var path, err = os.Getwd()
-			if err != nil {
-				zlog.Fatal().Err(err).Msg("error while finding working directory")
-				return 1
-			}
-			path, err = dryad.StemPath(path)
+	type ParsedArgs struct {
+		Parallel int
+	}
+
+	var parseArgs task.Task[clib.ActionRequest, ParsedArgs] = func(ctx *task.ExecutionContext, req clib.ActionRequest) (error, ParsedArgs) {
+		var options = req.Opts
+
+		var parallel int
+		if options["parallel"] != nil {
+			parallel = int(options["parallel"].(int64))
+		} else {
+			parallel = PARALLEL_COUNT_DEFAULT
+		}
+
+		return nil, ParsedArgs{
+			Parallel: parallel,
+		}
+	}
+
+	var printStemPath = func(ctx *task.ExecutionContext, args ParsedArgs) (error, any) {
+		path, err := os.Getwd()
+		if err != nil {
+			return err, nil
+		}
+
+		path, err = dryad.StemPath(path)
+		if err != nil {
+			return err, nil
+		}
+
+		fmt.Println(path)
+		return nil, nil
+	}
+
+	printStemPath = task.WithContext(
+		printStemPath,
+		func(ctx *task.ExecutionContext, args ParsedArgs) (error, *task.ExecutionContext) {
+			return nil, task.NewContext(args.Parallel)
+		},
+	)
+
+	var action = task.Return(
+		task.Series2(
+			parseArgs,
+			printStemPath,
+		),
+		func(err error, val any) int {
 			if err != nil {
 				zlog.Fatal().Err(err).Msg("error while finding stem path")
 				return 1
 			}
-			fmt.Println(path)
 
 			return 0
-		})
+		},
+	)
 
+	command := clib.NewCommand("path", "return the base path of the current root").
+		WithAction(action)
+
+	command = ParallelCommand(command)
 	command = LoggingCommand(command)
-
 
 	return command
 }()
