8d6aee7f7eb56d42b0ade31aa3aa803b1413dcef
James L <somesocks@gmail.com>
2026-02-06 17:07:08 +0100

    - dyd/roots/components/dryad-cli/source - refactoring scope use command to task pipeline with --parallel context support

--- dyd/roots/components/dryad-cli/source/dyd/assets/cli/scope-use.go
+++ dyd/roots/components/dryad-cli/source/dyd/assets/cli/scope-use.go
@@ -10,46 +10,91 @@ import (
 )
 
 var scopeUseCommand = func() clib.Command {
+	type ParsedArgs struct {
+		Name       string
+		GardenPath string
+		Parallel   int
+	}
+
+	var parseArgs = task.From(
+		func(req clib.ActionRequest) (error, ParsedArgs) {
+			var args = req.Args
+			var opts = req.Opts
+
+			var name string = args[0]
+			var parallel int
+
+			if opts["parallel"] != nil {
+				parallel = int(opts["parallel"].(int64))
+			} else {
+				parallel = PARALLEL_COUNT_DEFAULT
+			}
+
+			var path, err = os.Getwd()
+			if err != nil {
+				return err, ParsedArgs{}
+			}
+
+			return nil, ParsedArgs{
+				Name:       name,
+				GardenPath: path,
+				Parallel:   parallel,
+			}
+		},
+	)
+
+	var useScope = func(ctx *task.ExecutionContext, args ParsedArgs) (error, any) {
+		unsafeGarden := dryad.Garden(args.GardenPath)
+
+		err, garden := unsafeGarden.Resolve(ctx)
+		if err != nil {
+			return err, nil
+		}
+
+		if args.Name == "none" {
+			err = dryad.ScopeUnsetDefault(garden)
+		} else {
+			err = dryad.ScopeSetDefault(garden, args.Name)
+		}
+
+		if err != nil {
+			return err, nil
+		}
+
+		return nil, nil
+	}
+
+	useScope = task.WithContext(
+		useScope,
+		func(ctx *task.ExecutionContext, args ParsedArgs) (error, *task.ExecutionContext) {
+			return nil, task.NewContext(args.Parallel)
+		},
+	)
+
+	var action = task.Return(
+		task.Series2(
+			parseArgs,
+			useScope,
+		),
+		func(err error, val any) int {
+			if err != nil {
+				zlog.Fatal().Err(err).Msg("error while setting active scope")
+				return 1
+			}
+			return 0
+		},
+	)
+
 	command := clib.NewCommand("use", "set a scope to be active. alias for `dryad scopes default set`").
 		WithArg(
 			clib.
 				NewArg("name", "the name of the scope to set as active. use 'none' to unset the active scope").
 				WithAutoComplete(ArgAutoCompleteScope),
 		).
-		WithAction(func(req clib.ActionRequest) int {
-			var args = req.Args
+		WithAction(action)
 
-			var name string = args[0]
-
-			var path, err = os.Getwd()
-			if err != nil {
-				zlog.Fatal().Err(err).Msg("error while finding working directory")
-				return 1
-			}
-
-			unsafeGarden := dryad.Garden(path)
-			
-			err, garden := unsafeGarden.Resolve(task.SERIAL_CONTEXT)
-			if err != nil {
-				return 1
-			}
-
-			if name == "none" {
-				err = dryad.ScopeUnsetDefault(garden)
-			} else {
-				err = dryad.ScopeSetDefault(garden, name)
-			}
-
-			if err != nil {
-				zlog.Fatal().Err(err).Msg("error while setting active scope")
-				return 1
-			}
-
-			return 0
-		})
-
+	command = ParallelCommand(command)
 	command = LoggingCommand(command)
-
 
 	return command
 }()
