71217e2b90fa35721f41af31201426badee9d74e
James L <somesocks@gmail.com>
2026-02-06 17:33:37 +0100

    - dyd/roots/components/dryad-cli/source - refactoring scopes default unset command to task pipeline with --parallel context support

--- dyd/roots/components/dryad-cli/source/dyd/assets/cli/scopes-default-unset.go
+++ dyd/roots/components/dryad-cli/source/dyd/assets/cli/scopes-default-unset.go
@@ -10,32 +10,76 @@ import (
 )
 
 var scopesDefaultUnsetCommand = func() clib.Command {
-	command := clib.NewCommand("unset", "remove the default scope setting").
-		WithAction(func(req clib.ActionRequest) int {
+	type ParsedArgs struct {
+		GardenPath string
+		Parallel   int
+	}
+
+	var parseArgs = task.From(
+		func(req clib.ActionRequest) (error, ParsedArgs) {
+			var opts = req.Opts
+			var parallel int
+
+			if opts["parallel"] != nil {
+				parallel = int(opts["parallel"].(int64))
+			} else {
+				parallel = PARALLEL_COUNT_DEFAULT
+			}
+
 			var path, err = os.Getwd()
 			if err != nil {
-				zlog.Fatal().Err(err).Msg("error while finding active directory")
-				return 1
+				return err, ParsedArgs{}
 			}
 
-			unsafeGarden := dryad.Garden(path)
-			
-			err, garden := unsafeGarden.Resolve(task.SERIAL_CONTEXT)
-			if err != nil {
-				return 1
+			return nil, ParsedArgs{
+				GardenPath: path,
+				Parallel:   parallel,
 			}
+		},
+	)
 
-			err = dryad.ScopeUnsetDefault(garden)
+	var unsetDefaultScope = func(ctx *task.ExecutionContext, args ParsedArgs) (error, any) {
+		unsafeGarden := dryad.Garden(args.GardenPath)
+
+		err, garden := unsafeGarden.Resolve(ctx)
+		if err != nil {
+			return err, nil
+		}
+
+		err = dryad.ScopeUnsetDefault(garden)
+		if err != nil {
+			return err, nil
+		}
+
+		return nil, nil
+	}
+
+	unsetDefaultScope = task.WithContext(
+		unsetDefaultScope,
+		func(ctx *task.ExecutionContext, args ParsedArgs) (error, *task.ExecutionContext) {
+			return nil, task.NewContext(args.Parallel)
+		},
+	)
+
+	var action = task.Return(
+		task.Series2(
+			parseArgs,
+			unsetDefaultScope,
+		),
+		func(err error, val any) int {
 			if err != nil {
 				zlog.Fatal().Err(err).Msg("error while removing active scope")
 				return 1
 			}
-
 			return 0
-		})
+		},
+	)
 
+	command := clib.NewCommand("unset", "remove the default scope setting").
+		WithAction(action)
+
+	command = ParallelCommand(command)
 	command = LoggingCommand(command)
-
 
 	return command
 }()
