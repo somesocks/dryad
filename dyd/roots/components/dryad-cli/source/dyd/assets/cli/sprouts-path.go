4297977b35fe399f243748c3eacd72f4af818498
James L <somesocks@gmail.com>
2026-02-06 18:20:26 +0100

    refactor(cli): parallelize sprouts path

--- dyd/roots/components/dryad-cli/source/dyd/assets/cli/sprouts-path.go
+++ dyd/roots/components/dryad-cli/source/dyd/assets/cli/sprouts-path.go
@@ -10,34 +10,79 @@ import (
 	zlog "github.com/rs/zerolog/log"
 )
 
-var sproutsPathCommand = func() clib.Command {
-	command := clib.NewCommand("path", "return the path of the sprouts dir").
-		WithAction(func(req clib.ActionRequest) int {
-			var path, err = os.Getwd()
-			if err != nil {
-				zlog.Fatal().Err(err).Msg("error while finding working directory")
-				return 1
+var sproutsPathAction = func(req clib.ActionRequest) int {
+	type ParsedArgs struct {
+		GardenPath string
+		Parallel   int
+	}
+
+	var parseArgs = task.From(
+		func(req clib.ActionRequest) (error, ParsedArgs) {
+			var opts = req.Opts
+			var parallel int
+
+			if opts["parallel"] != nil {
+				parallel = int(opts["parallel"].(int64))
+			} else {
+				parallel = PARALLEL_COUNT_DEFAULT
 			}
 
-			err, garden := dryad.Garden(path).Resolve(task.SERIAL_CONTEXT)
+			path, err := os.Getwd()
 			if err != nil {
-				zlog.Fatal().Err(err).Msg("error resolving garden")
-				return 1
+				return err, ParsedArgs{}
 			}
 
-			err, sprouts := garden.Sprouts().Resolve(task.SERIAL_CONTEXT)
+			return nil, ParsedArgs{
+				GardenPath: path,
+				Parallel:   parallel,
+			}
+		},
+	)
+
+	var printSproutsPath = func(ctx *task.ExecutionContext, args ParsedArgs) (error, any) {
+		err, garden := dryad.Garden(args.GardenPath).Resolve(ctx)
+		if err != nil {
+			return err, nil
+		}
+
+		err, sprouts := garden.Sprouts().Resolve(ctx)
+		if err != nil {
+			return err, nil
+		}
+
+		fmt.Println(sprouts.BasePath)
+		return nil, nil
+	}
+
+	printSproutsPath = task.WithContext(
+		printSproutsPath,
+		func(ctx *task.ExecutionContext, args ParsedArgs) (error, *task.ExecutionContext) {
+			return nil, task.NewContext(args.Parallel)
+		},
+	)
+
+	return task.Return(
+		task.Series2(
+			parseArgs,
+			printSproutsPath,
+		),
+		func(err error, val any) int {
 			if err != nil {
-				zlog.Fatal().Err(err).Msg("error resolving sprouts")
+				zlog.Fatal().Err(err).Msg("error while finding sprouts path")
 				return 1
 			}
-
-			fmt.Println(sprouts.BasePath)
 
 			return 0
-		})
+		},
+	)(req)
+}
 
+var sproutsPathCommand = func() clib.Command {
+	command := clib.NewCommand("path", "return the path of the sprouts dir").
+		WithAction(sproutsPathAction)
+
+	command = ParallelCommand(command)
 	command = LoggingCommand(command)
-
 
 	return command
 }()
