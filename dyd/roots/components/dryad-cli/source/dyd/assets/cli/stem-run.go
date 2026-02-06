f71b2d456c58cd6b8c6696cf46387ab101efa8e1
James L <somesocks@gmail.com>
2026-02-06 19:43:17 +0100

    refactor(cli): parallelize stem run

--- dyd/roots/components/dryad-cli/source/dyd/assets/cli/stem-run.go
+++ dyd/roots/components/dryad-cli/source/dyd/assets/cli/stem-run.go
@@ -13,6 +13,133 @@ import (
 )
 
 var stemRunCommand = func() clib.Command {
+	type ParsedArgs struct {
+		Path     string
+		Context  string
+		Override string
+		Inherit  bool
+		Confirm  string
+		Extras   []string
+		Parallel int
+	}
+
+	var parseArgs task.Task[clib.ActionRequest, ParsedArgs] = func(ctx *task.ExecutionContext, req clib.ActionRequest) (error, ParsedArgs) {
+		var args = req.Args
+		var options = req.Opts
+
+		var context string
+		var override string
+		var inherit bool
+		var confirm string
+		var parallel int
+
+		if options["context"] != nil {
+			context = options["context"].(string)
+		}
+
+		if options["inherit"] != nil {
+			inherit = options["inherit"].(bool)
+		}
+
+		if options["override"] != nil {
+			override = options["override"].(string)
+		}
+
+		if options["confirm"] != nil {
+			confirm = options["confirm"].(string)
+		}
+
+		if options["parallel"] != nil {
+			parallel = int(options["parallel"].(int64))
+		} else {
+			parallel = PARALLEL_COUNT_DEFAULT
+		}
+
+		return nil, ParsedArgs{
+			Path:     args[0],
+			Context:  context,
+			Override: override,
+			Inherit:  inherit,
+			Confirm:  confirm,
+			Extras:   args[1:],
+			Parallel: parallel,
+		}
+	}
+
+	var runStem = func(ctx *task.ExecutionContext, args ParsedArgs) (error, any) {
+		if args.Confirm != "" {
+			fmt.Println("this package will be executed:")
+			fmt.Println(args.Path)
+			fmt.Println("are you sure? type '" + args.Confirm + "' to continue")
+
+			reader := bufio.NewReader(os.Stdin)
+			input, err := reader.ReadString('\n')
+			if err != nil {
+				return err, nil
+			}
+
+			input = strings.TrimSuffix(input, "\n")
+
+			if input != args.Confirm {
+				return fmt.Errorf("input does not match confirmation, aborting"), nil
+			}
+		}
+
+		var env = map[string]string{}
+
+		if args.Inherit {
+			for _, e := range os.Environ() {
+				if i := strings.Index(e, "="); i >= 0 {
+					env[e[:i]] = e[i+1:]
+				}
+			}
+		} else {
+			env["TERM"] = os.Getenv("TERM")
+		}
+
+		err, garden := dryad.Garden(args.Path).Resolve(ctx)
+		if err != nil {
+			return err, nil
+		}
+
+		err = dryad.StemRun(dryad.StemRunRequest{
+			Garden:       garden,
+			StemPath:     args.Path,
+			MainOverride: args.Override,
+			Env:          env,
+			Args:         args.Extras,
+			JoinStdout:   true,
+			Context:      args.Context,
+		})
+		if err != nil {
+			return err, nil
+		}
+
+		return nil, nil
+	}
+
+	runStem = task.WithContext(
+		runStem,
+		func(ctx *task.ExecutionContext, args ParsedArgs) (error, *task.ExecutionContext) {
+			return nil, task.NewContext(args.Parallel)
+		},
+	)
+
+	var action = task.Return(
+		task.Series2(
+			parseArgs,
+			runStem,
+		),
+		func(err error, val any) int {
+			if err != nil {
+				zlog.Fatal().Err(err).Msg("error executing stem")
+				return 1
+			}
+
+			return 0
+		},
+	)
+
 	command := clib.NewCommand("run", "execute the main for a stem").
 		WithArg(
 			clib.
@@ -24,99 +151,10 @@ var stemRunCommand = func() clib.Command {
 		WithOption(clib.NewOption("override", "run this executable in the stem run envinronment instead of the main")).
 		WithOption(clib.NewOption("confirm", "ask for a confirmation string to be entered to execute this command").WithType(clib.OptionTypeString)).
 		WithArg(clib.NewArg("-- args", "args to pass to the stem").AsOptional()).
-		WithAction(func(req clib.ActionRequest) int {
-			var args = req.Args
-			var options = req.Opts
+		WithAction(action)
 
-			var context string
-			var override string
-			var inherit bool
-			var confirm string
-
-			if options["context"] != nil {
-				context = options["context"].(string)
-			}
-
-			if options["inherit"] != nil {
-				inherit = options["inherit"].(bool)
-			}
-
-			if options["override"] != nil {
-				override = options["override"].(string)
-			}
-
-			if options["confirm"] != nil {
-				confirm = options["confirm"].(string)
-			}
-
-			// if confirm is set, we want to print the list
-			// of sprouts to run
-			if confirm != "" {
-				fmt.Println("this package will be executed:")
-				fmt.Println(args[0])
-				fmt.Println("are you sure? type '" + confirm + "' to continue")
-
-				reader := bufio.NewReader(os.Stdin)
-
-				input, err := reader.ReadString('\n')
-				if err != nil {
-					zlog.Fatal().Err(err).Msg("error while reading input")
-					return 1
-				}
-
-				input = strings.TrimSuffix(input, "\n")
-
-				if input != confirm {
-					zlog.Fatal().Msg("input does not match confirmation, aborting")
-					return 1
-				}
-
-			}
-
-			var env = map[string]string{}
-
-			// pull
-			if inherit {
-				for _, e := range os.Environ() {
-					if i := strings.Index(e, "="); i >= 0 {
-						env[e[:i]] = e[i+1:]
-					}
-				}
-			} else {
-				// copy a few variables over from parent env for convenience
-				env["TERM"] = os.Getenv("TERM")
-			}
-
-			path := args[0]
-			extras := args[1:]
-
-			unsafeGarden := dryad.Garden(path)
-			
-			err, garden := unsafeGarden.Resolve(task.SERIAL_CONTEXT)
-			if err != nil {
-				zlog.Fatal().Err(err).Msg("error resolving garden")
-				return 1
-			}
-
-			err = dryad.StemRun(dryad.StemRunRequest{
-				Garden: garden,
-				StemPath:     path,
-				MainOverride: override,
-				Env:          env,
-				Args:         extras,
-				JoinStdout:   true,
-				Context:      context,
-			})
-			if err != nil {
-				zlog.Fatal().Err(err).Msg("error executing stem")
-				return 1
-			}
-
-			return 0
-		})
-
+	command = ParallelCommand(command)
 	command = LoggingCommand(command)
-
 
 	return command
 }()
