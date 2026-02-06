dfdcc3fb10f1b307fca1fdb2639e94f69e025288
James L <somesocks@gmail.com>
2026-02-06 16:41:45 +0100

    - dyd/roots/components/dryad-cli/source - refactoring root requirements list command to task pipeline with --parallel context support

--- dyd/roots/components/dryad-cli/source/dyd/assets/cli/root-requirements-list.go
+++ dyd/roots/components/dryad-cli/source/dyd/assets/cli/root-requirements-list.go
@@ -3,8 +3,8 @@ package cli
 import (
 	clib "dryad/cli-builder"
 	dryad "dryad/core"
-	"dryad/task"
 	dydfs "dryad/filesystem"
+	"dryad/task"
 	"fmt"
 	"path/filepath"
 
@@ -12,6 +12,129 @@ import (
 )
 
 var rootRequirementsListCommand = func() clib.Command {
+	type ParsedArgs struct {
+		RootPath string
+		Relative bool
+		Parallel int
+	}
+
+	var parseArgs = func(ctx *task.ExecutionContext, req clib.ActionRequest) (error, ParsedArgs) {
+		var args = req.Args
+		var options = req.Opts
+		var rootPath string
+		var err error
+
+		if len(args) > 0 {
+			rootPath = args[0]
+		}
+
+		var relative bool
+
+		if options["relative"] != nil {
+			relative = options["relative"].(bool)
+		} else {
+			relative = true
+		}
+
+		var parallel int
+		if options["parallel"] != nil {
+			parallel = int(options["parallel"].(int64))
+		} else {
+			parallel = PARALLEL_COUNT_DEFAULT
+		}
+
+		err, rootPath = dydfs.PartialEvalSymlinks(ctx, rootPath)
+		if err != nil {
+			return err, ParsedArgs{}
+		}
+
+		return nil, ParsedArgs{
+			RootPath: rootPath,
+			Relative: relative,
+			Parallel: parallel,
+		}
+	}
+
+	var listRequirements = func(ctx *task.ExecutionContext, args ParsedArgs) (error, any) {
+		err, garden := dryad.Garden(args.RootPath).Resolve(ctx)
+		if err != nil {
+			return err, nil
+		}
+
+		err, roots := garden.Roots().Resolve(ctx)
+		if err != nil {
+			return err, nil
+		}
+
+		err, safeRoot := roots.Root(args.RootPath).Resolve(ctx)
+		if err != nil {
+			return err, nil
+		}
+
+		err, safeRequirements := safeRoot.Requirements().Resolve(ctx)
+		if err != nil {
+			return err, nil
+		} else if safeRequirements == nil {
+			// no requirements, so exit
+			return nil, nil
+		}
+
+		var onRequirementMatch = func(ctx *task.ExecutionContext, requirement *dryad.SafeRootRequirementReference) (error, any) {
+			zlog.Trace().
+				Str("path", requirement.BasePath).
+				Msg("root requirements list / onRequirement")
+
+			if args.Relative {
+				// calculate the relative path to the root from the base of the garden
+				relPath, err := filepath.Rel(
+					requirement.Requirements.Root.Roots.Garden.BasePath,
+					requirement.BasePath,
+				)
+				if err != nil {
+					return err, nil
+				}
+				fmt.Println(relPath)
+			} else {
+				fmt.Println(requirement.BasePath)
+			}
+			return nil, nil
+		}
+
+		err = safeRequirements.Walk(
+			ctx,
+			dryad.RootRequirementsWalkRequest{
+				OnMatch: onRequirementMatch,
+			},
+		)
+		if err != nil {
+			return err, nil
+		}
+
+		return nil, nil
+	}
+
+	listRequirements = task.WithContext(
+		listRequirements,
+		func(ctx *task.ExecutionContext, args ParsedArgs) (error, *task.ExecutionContext) {
+			return nil, task.NewContext(args.Parallel)
+		},
+	)
+
+	var action = task.Return(
+		task.Series2(
+			parseArgs,
+			listRequirements,
+		),
+		func(err error, val any) int {
+			if err != nil {
+				zlog.Fatal().Err(err).Msg("error while crawling root requirements")
+				return 1
+			}
+
+			return 0
+		},
+	)
+
 	command := clib.NewCommand("list", "list all requirements of this root").
 		WithArg(
 			clib.
@@ -20,95 +143,10 @@ var rootRequirementsListCommand = func() clib.Command {
 				WithAutoComplete(ArgAutoCompletePath),
 		).
 		WithOption(clib.NewOption("relative", "print roots relative to the base garden path. default true").WithType(clib.OptionTypeBool)).
-		WithAction(func(req clib.ActionRequest) int {
-			var args = req.Args
-			var options = req.Opts
-			var rootPath string
-			var err error
+		WithAction(action)
 
-			if len(args) > 0 {
-				rootPath = args[0]
-			}
-
-			var relative bool = true
-
-			if options["relative"] != nil {
-				relative = options["relative"].(bool)
-			} else {
-				relative = true
-			}
-
-			err, rootPath = dydfs.PartialEvalSymlinks(task.SERIAL_CONTEXT, rootPath)
-			if err != nil {
-				zlog.Fatal().Err(err).Msg("error resolving root path")
-				return 1
-			}
-
-			err, garden := dryad.Garden(rootPath).Resolve(task.SERIAL_CONTEXT)
-			if err != nil {
-				zlog.Fatal().Err(err).Msg("error resolving garden")
-				return 1
-			}
-
-			err, roots := garden.Roots().Resolve(task.SERIAL_CONTEXT)
-			if err != nil {
-				zlog.Fatal().Err(err).Msg("error resolving garden roots")
-				return 1
-			}
-	
-			err, safeRoot := roots.Root(rootPath).Resolve(task.SERIAL_CONTEXT)
-			if err != nil {
-				zlog.Fatal().Err(err).Msg("error while resolving root")
-				return 1
-			}
-
-			err, safeRequirements := safeRoot.Requirements().Resolve(task.SERIAL_CONTEXT)
-			if err != nil {
-				zlog.Fatal().Err(err).Msg("error while resolving root requirements")
-				return 1
-			} else if safeRequirements == nil {
-				// no requirements, so exit
-				return 0				
-			}
-
-			var onRequirementMatch =func (ctx *task.ExecutionContext, requirement *dryad.SafeRootRequirementReference) (error, any) {
-				zlog.Trace().
-					Str("path", requirement.BasePath).
-					Msg("root requirements list / onRequirement")				
-
-				if relative {
-					// calculate the relative path to the root from the base of the garden
-					relPath, err := filepath.Rel(
-						requirement.Requirements.Root.Roots.Garden.BasePath,
-						requirement.BasePath,
-					)
-					if err != nil {
-						return err, nil
-					}
-					fmt.Println(relPath)
-				} else {
-					fmt.Println(requirement.BasePath)
-				}
-				return nil, nil
-			}
-
-			err = safeRequirements.Walk(
-				task.SERIAL_CONTEXT,
-				dryad.RootRequirementsWalkRequest{
-					OnMatch: onRequirementMatch,
-				},
-			)
-
-			if err != nil {
-				zlog.Fatal().Err(err).Msg("error while crawling root requirements")
-				return 1
-			}
-
-			return 0
-		})
-
+	command = ParallelCommand(command)
 	command = LoggingCommand(command)
-
 
 	return command
 }()
