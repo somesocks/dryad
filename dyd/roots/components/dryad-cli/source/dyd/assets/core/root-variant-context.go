048c5a12d745649f8d6e649edc79be39e406b763
James L <somesocks@gmail.com>
2026-04-08 23:14:40 +0200

    Add variant-qualified refs to root ancestors

--- dyd/roots/components/dryad-cli/source/dyd/assets/core/root-variant-context.go
+++ dyd/roots/components/dryad-cli/source/dyd/assets/core/root-variant-context.go
@@ -22,12 +22,38 @@ func RootVariantContextFromFilesystem(raw string) (error, RootVariantContext) {
 	return nil, RootVariantContext{Descriptor: descriptor}
 }
 
+func RootVariantContextFromURL(raw string) (error, RootVariantContext) {
+	err, normalized := variantDescriptorNormalizeURL(raw)
+	if err != nil {
+		return err, RootVariantContext{}
+	}
+
+	err, descriptor := variantDescriptorParseURL(normalized)
+	if err != nil {
+		return err, RootVariantContext{}
+	}
+
+	return nil, RootVariantContext{Descriptor: descriptor}
+}
+
 func (context RootVariantContext) Filesystem() (error, string) {
 	return variantDescriptorEncodeFilesystem(context.Descriptor)
 }
 
 func (context RootVariantContext) MustFilesystem() string {
 	err, encoded := context.Filesystem()
+	if err != nil {
+		panic(fmt.Sprintf("invalid root variant context: %v", err))
+	}
+	return encoded
+}
+
+func (context RootVariantContext) URL() (error, string) {
+	return variantDescriptorEncodeURL(context.Descriptor)
+}
+
+func (context RootVariantContext) MustURL() string {
+	err, encoded := context.URL()
 	if err != nil {
 		panic(fmt.Sprintf("invalid root variant context: %v", err))
 	}
